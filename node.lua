5b018e476aa9fb5be2302fd5bbc0cb083b6108e3
Florian Wesch <fw@dividuum.de>
2019-08-27 18:33:39 +0200

    use os.time for synced playback

--- node.lua
+++ node.lua
@@ -8,7 +8,12 @@ util.resource_loader{
 function node.render()
     gl.clear(1,1,1,1)
     shader:use{
-        Effect = math.cos(sys.now()*2)*3
+        -- This uses local time (seconds since info-beamer started on this Pi)
+        -- Effect = math.cos(sys.now()*2)*3
+
+        -- This uses os.time(), the real time. As a side effect, all Pis with
+        -- proper synced time will have the effect synced.
+        Effect = math.cos(os.time()*2)*3
     }
     lua:draw(120, 40, 520, 440)
 end
